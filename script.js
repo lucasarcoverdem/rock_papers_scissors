32edbbbd540ff52b3301c2c37de6a72706b2964f
Lucas Arcoverde de Melo <lucasarcoverdem@gmail.com>
2025-04-03 22:48:56 -0300

    Improves code writing

--- script.js
+++ script.js
@@ -1,74 +1,59 @@
-const rockButton = document.getElementById('rockButton');
-const paperButton = document.getElementById('paperButton');
-const scissorsButton = document.getElementById('scissorsButton');
-const resetScoreBtn = document.getElementById('resetScoreBtn');
-let scoreParagraph = document.getElementById('scoreParagraph');
-let resultParagraph = document.getElementById('result');
+const rockButton = document.getElementById('js-rock-button');
+const paperButton = document.getElementById('js-paper-button');
+const scissorsButton = document.getElementById('js-scissors-button');
+const resetScoreBtn = document.getElementById('js-reset-score');
+const scoreParagraph = document.getElementById('js-score');
+const resultParagraph = document.getElementById('js-result');
 let result;
 
 let score = JSON.parse(localStorage.getItem('score')) || {
     win: 0,
     defeat: 0,
     tie: 0
-}
+};
 
 function pickComputerMove() {
     const randomNumber = Math.random();
-    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    if (randomNumber < 1 / 3) {
         return 'Rock';
-    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    } else if (randomNumber < 2 / 3) {
         return 'Paper';
-    } else if (randomNumber >= 2 / 3) {
+    } else {
         return 'Scissors';
     }
 }
 
 function playGame(playerMove) {
-    const computerMove = pickComputerMove()
+    const computerMove = pickComputerMove();
 
     if (playerMove === computerMove) {
         result = 'Tie';
         score.tie++;
-    } else if (playerMove === 'Rock') {
-        if (computerMove === 'Paper') {
-            result = 'Defeat';
-            score.defeat++;
-        } else if (computerMove === 'Scissors') {
-            result = 'Win';
-            score.win++;
-        }
-    } else if (playerMove === 'Paper') {
-        if (computerMove === 'Rock') {
-            result = 'Win';
-            score.win++;
-        } else if (computerMove === 'Scissors') {
-            result = 'Defeat';
-            score.defeat++;
-        }
-    } else if (playerMove === 'Scissors') {
-        if (computerMove === 'Rock') {
-            result = 'Defeat';
-            score.defeat++;
-        } else if (computerMove === 'Paper') {
-            result = 'Win';
-            score.win++;
-        }
+    } else if (
+        (playerMove === 'Rock' && computerMove === 'Scissors') ||
+        (playerMove === 'Paper' && computerMove === 'Rock') ||
+        (playerMove === 'Scissors' && computerMove === 'Paper')
+    ) {
+        result = 'Win';
+        score.win++;
+    } else {
+        result = 'Defeat';
+        score.defeat++;
     }
 
     localStorage.setItem('score', JSON.stringify(score));
 
-    resultParagraph.innerHTML = `You: ${playerMove} | CPU: ${computerMove} | ${result} `
-    scoreParagraph.innerHTML = `Wins: ${score.win} | Defeats: ${score.defeat} | Ties: ${score.tie}`
+    resultParagraph.innerHTML = `You: ${playerMove} | CPU: ${computerMove} | ${result}`;
+    scoreParagraph.innerHTML = `Wins: ${score.win} | Defeats: ${score.defeat} | Ties: ${score.tie}`;
 }
 
 rockButton.onclick = () => playGame('Rock');
 paperButton.onclick = () => playGame('Paper');
 scissorsButton.onclick = () => playGame('Scissors');
+
 resetScoreBtn.onclick = () => {
-    score.win = 0;
-    score.defeat = 0;
-    score.tie = 0;
+    score = { win: 0, defeat: 0, tie: 0 };
     localStorage.removeItem('score');
-    scoreParagraph.innerHTML = `Wins: ${score.win} | Defeats: ${score.defeat} | Ties: ${score.tie}`
-    resultParagraph.innerHTML = 'Choose your move!'
-}
+    scoreParagraph.innerHTML = `Wins: 0 | Defeats: 0 | Ties: 0`;
+    resultParagraph.innerHTML = 'Choose your move!';
+};
